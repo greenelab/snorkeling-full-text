f525fc4679e324fc6b4e3478c7ca20276c3512a4
David Nicholson <dnicholson329@gmail.com>
2021-12-01 13:58:42 -0500

    added more helper functions

--- snorkeling_helper/candidates_helper/encode_candidates_modules.py
+++ snorkeling_helper/candidates_helper/encode_candidates_modules.py
@@ -26,41 +26,41 @@ def encode_lemmas(
     for idx, row in tqdm.tqdm(candidate_df.iterrows()):
         lemma_array = row.lemma.copy()
 
-        if row.disease_end < row.gene_start:
+        if row[entity_fieldnames[1]] < row[entity_fieldnames[2]]:
             offset = row[entity_fieldnames[1]] - row[entity_fieldnames[0]]
-            lemma_array[row[entity_fieldnames[0]]: row[entity_fieldnames[1]]] = [
+            lemma_array[row[entity_fieldnames[0]] : row[entity_fieldnames[1]]] = [
                 entity_one
             ]
             lemma_array[
                 row[entity_fieldnames[2]]
                 - offset
-                + 1: row[entity_fieldnames[2]]
+                + 1 : row[entity_fieldnames[2]]
                 - offset
                 + 1
             ] = [entity_two]
 
         else:
             offset = row[entity_fieldnames[3]] - row[entity_fieldnames[2]]
-            lemma_array[row[entity_fieldnames[2]]: row[entity_fieldnames[3]]] = [
+            lemma_array[row[entity_fieldnames[2]] : row[entity_fieldnames[3]]] = [
                 entity_two
             ]
             lemma_array[
                 row[entity_fieldnames[0]]
                 - offset
-                + 1: row[entity_fieldnames[1]]
+                + 1 : row[entity_fieldnames[1]]
                 - offset
                 + 1
             ] = [entity_one]
 
         filtered_lemma = list(filter(lambda x: x not in stopwords, lemma_array))
 
-        encoded_array = list(
-            map(lambda x: word_mapper[x] if x in word_mapper else 1, filtered_lemma)
-        )
+        # encoded_array = list(
+        #    map(lambda x: word_mapper[x] if x in word_mapper else 1, filtered_lemma)
+        # )
 
         embedded_sentences.append(
             {
-                "encoded_lemmas": "|".join(map(str, encoded_array)),
+                # "encoded_lemmas": "|".join(map(str, encoded_array)),
                 "parsed_lemmas": "|".join(filtered_lemma),
                 "candidate_id": row.candidate_id,
             }
