b28549f781a8c696b3e2761e336ae8bc993803e3
David Nicholson <dnicholson329@gmail.com>
2022-04-26 10:25:40 -0400

    uploading all label functions

---
+++ generative_model_training_all_labels/02_generative_model_ctd_all_labels.py
@@ -0,0 +1,201 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.9.1+dev
+#   kernelspec:
+#     display_name: Python [conda env:snorkeling_full_text]
+#     language: python
+#     name: conda-env-snorkeling_full_text-py
+# ---
+
+# # Does resampling experiment help with predicting CtD sentences?
+
+# +
+from pathlib import Path
+import warnings
+
+import numpy as np
+import pandas as pd
+import plydata as ply
+from sqlalchemy import create_engine
+
+from snorkel.labeling.analysis import LFAnalysis
+from snorkeling_helper.generative_model_helper import (
+    sample_lfs,
+    run_generative_label_function_sampler,
+)
+
+warnings.filterwarnings("ignore")
+# -
+
+username = "danich1"
+password = "snorkel"
+dbname = "pubmed_central_db"
+database_str = (
+    f"postgresql+psycopg2://{username}:{password}@/{dbname}?host=/var/run/postgresql"
+)
+conn = create_engine(database_str)
+
+# ## Load the data
+
+label_candidates_dir = Path("../label_candidates/output")
+notebook_output_dir = Path("../generative_model_training/output/CtD")
+
+# +
+L_abstracts = pd.read_csv(
+    str(label_candidates_dir / Path("cd_abstract_train_candidates_resampling.tsv")),
+    sep="\t",
+)
+
+print(L_abstracts.shape)
+L_abstracts.head().T
+# -
+
+L_dev = pd.read_csv(
+    str(label_candidates_dir / Path("cd_dev_test_candidates_resampling.tsv")), sep="\t"
+) >> ply.query("split==10")
+print(L_dev.shape)
+L_dev.head().T
+
+L_test = pd.read_csv(
+    str(label_candidates_dir / Path("cd_dev_test_candidates_resampling.tsv")), sep="\t"
+) >> ply.query("split==11")
+print(L_test.shape)
+L_test.head().T
+
+# ## Resort the Candidates Based on Abstract
+
+# Grab the document ids for resampling
+sql = """
+select cd_candidates.sentence_id, document_id, cd_candidates.candidate_id
+from sentence
+inner join (
+  select candidate.candidate_id, compound_disease.sentence_id from compound_disease
+  inner join candidate on candidate.candidate_id=compound_disease.candidate_id
+  ) as cd_candidates
+on sentence.sentence_id = cd_candidates.sentence_id
+"""
+candidate_doc_df = pd.read_sql(sql, database_str)
+candidate_doc_df.head()
+
+# +
+dev_test_ids = (
+    L_dev >> ply.select("document_id") >> ply.distinct() >> ply.pull("document_id")
+)
+
+filtered_candidate_id = (
+    candidate_doc_df
+    >> ply.query(f"document_id in {list(dev_test_ids)}")
+    >> ply.pull("candidate_id")
+)
+# -
+
+sorted_train_df = pd.read_csv(
+    str(notebook_output_dir / Path("ctd_dataset_mapper.tsv")), sep="\t"
+)
+sorted_train_df.head()
+
+trained_documents = (
+    sorted_train_df
+    >> ply.inner_join(candidate_doc_df, on="document_id")
+    >> ply.query("dataset=='train'")
+    >> ply.pull("candidate_id")
+)
+
+filtered_L_abstracts = L_abstracts >> ply.query(
+    f"candidate_id in {list(trained_documents)}"
+)
+print(filtered_L_abstracts.shape)
+filtered_L_abstracts.head()
+
+# ## Construct the Grid Search
+
+# Global Grid
+epochs_grid = [500]
+l2_param_grid = np.linspace(0.01, 5, num=5)
+lr_grid = [1e-2]
+grid = list(
+    zip(epochs_grid * len(l2_param_grid), l2_param_grid, lr_grid * len(l2_param_grid))
+)
+
+# # Abstracts
+
+# +
+analysis_module = LFAnalysis(
+    filtered_L_abstracts >> ply.select("candidate_id", drop=True)
+)
+
+abstract_lf_summary = analysis_module.lf_summary()
+abstract_lf_summary.index = (
+    filtered_L_abstracts >> ply.select("candidate_id", drop=True)
+).columns.tolist()
+
+abstract_lf_summary
+# -
+
+# # Set up fields for resampling
+
+lf_columns_base = list(L_abstracts.columns[0:2])
+candidate_id_field = list(L_abstracts.columns[-1:])
+dev_column_base = ["split", "curated_ctd", "document_id"]
+data_columns = []
+
+# # Abstracts - All Label Functions
+
+# ## CtD
+
+# +
+ctd_start = 2
+ctd_end = 102
+
+# Spaced out number of sampels including total
+size_of_samples = [1, 33, 65, 97, ctd_end - ctd_start]
+number_of_samples = 50
+ctd_lf_range = range(ctd_start, ctd_end)
+# -
+
+sampled_lfs_dict = {
+    sample_size: (
+        sample_lfs(
+            list(ctd_lf_range),
+            len(list(ctd_lf_range)),
+            sample_size,
+            number_of_samples,
+            random_state=100,
+        )
+    )
+    for sample_size in size_of_samples
+}
+
+data_columns += run_generative_label_function_sampler(
+    filtered_L_abstracts,
+    L_dev,
+    L_test,
+    sampled_lfs_dict,
+    lf_columns_base=lf_columns_base,
+    grid_param=grid,
+    marginals_df_file="",
+    curated_label="curated_ctd",
+    entity_label="ALL",
+    data_source="abstract",
+)
+
+# # Write Performance to File
+
+performance_df = pd.DataFrame.from_records(data_columns)
+performance_df
+
+(
+    performance_df
+    >> ply.call(
+        "to_csv",
+        str(Path("output") / Path("ALL_CtD_performance.tsv")),
+        index=False,
+        sep="\t",
+    )
+)
---
+++ generative_model_training_all_labels/05_plot_labels_sampling_performance_all_labels.py
@@ -0,0 +1,271 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.9.1+dev
+#   kernelspec:
+#     display_name: Python [conda env:snorkeling_full_text]
+#     language: python
+#     name: conda-env-snorkeling_full_text-py
+# ---
+
+# # Plot Label Resampling Performance
+
+# +
+from pathlib import Path
+
+import scipy.stats
+import pandas as pd
+import plotnine as p9
+import plydata as ply
+# -
+
+# ## Gather performance dataset files and manipulate the dataframes
+
+performance_dfs = pd.concat(
+    [
+        pd.read_csv(str(df_path), sep="\t")
+        >> ply.define(lf_num=ply.expressions.if_else("lf_num > 97", 99, "lf_num"))
+        >> ply.define(
+            lf_num=ply.expressions.case_when(
+                {
+                    "lf_num==1": "lf_num",
+                    "lf_num==33": "lf_num",
+                    "lf_num==65": "lf_num",
+                    "lf_num==97": "lf_num",
+                    "lf_num==99": "lf_num",
+                    True: 0,
+                }
+            )
+        )
+        >> ply.define(
+            prediction_label='"' + df_path.stem.split("_")[1] + '"',
+        )
+        for df_path in Path("output").rglob("*tsv")
+    ]
+)
+print(performance_dfs.shape)
+performance_dfs.head()
+
+# +
+entity_labels = (
+    performance_dfs
+    >> ply.select("label_source")
+    >> ply.distinct()
+    >> ply.pull("label_source")
+)
+
+data_rows = []
+for idx, row in performance_dfs >> ply.query("lf_num==0") >> ply.call(".iterrows"):
+
+    for entity in entity_labels:
+
+        if entity == row["label_source"]:
+            continue
+
+        data_rows.append(
+            {
+                "lf_num": 0,
+                "aupr": row["aupr"],
+                "auroc": row["auroc"],
+                "bce_loss": row["bce_loss"],
+                "sampeld_lf_name": row["sampled_lf_name"],
+                "label_source": entity,
+                "data_source": row["data_source"],
+                "prediction_label": row["prediction_label"],
+            }
+        )
+
+
+# +
+def upper_ci(x):
+    return x.mean() + (
+        scipy.stats.sem(x) * scipy.stats.t.ppf((1 + 0.95) / 2.0, len(x) - 1)
+    )
+
+
+def lower_ci(x):
+    return x.mean() - (
+        scipy.stats.sem(x) * scipy.stats.t.ppf((1 + 0.95) / 2.0, len(x) - 1)
+    )
+
+
+# -
+
+performance_ci_df = (
+    performance_dfs
+    >> ply.call(".append", pd.DataFrame.from_records(data_rows))
+    >> ply.group_by(
+        "lf_num", "label_source", "prediction_label", "data_source", "model"
+    )
+    >> ply.define(
+        aupr_mean="mean(aupr)",
+        aupr_upper_ci="upper_ci(aupr)",
+        aupr_lower_ci="lower_ci(aupr)",
+    )
+    >> ply.define(
+        auroc_mean="mean(auroc)",
+        auroc_upper_ci="upper_ci(auroc)",
+        auroc_lower_ci="lower_ci(auroc)",
+    )
+    >> ply.define(
+        bce_mean="mean(bce_loss)",
+        bce_upper_ci="upper_ci(bce_loss)",
+        bce_lower_ci="lower_ci(bce_loss)",
+    )
+    >> ply.ungroup()
+    >> ply.define(lf_sample=ply.expressions.if_else("lf_num==99", '"All"', "lf_num"))
+    >> ply.select(
+        "lf_num",
+        "label_source",
+        "prediction_label",
+        "data_source",
+        "model",
+        "aupr_mean",
+        "aupr_upper_ci",
+        "aupr_lower_ci",
+        "auroc_mean",
+        "auroc_upper_ci",
+        "auroc_lower_ci",
+        "bce_mean",
+        "bce_upper_ci",
+        "bce_lower_ci",
+    )
+    >> ply.call(".dropna")
+    >> ply.distinct()
+)
+performance_ci_df.lf_num = pd.Categorical(performance_ci_df.lf_num.astype(int).tolist())
+performance_ci_df >> ply.call(
+    "to_csv", "output/generative_model_all_lf_performance.tsv", sep="\t", index=False
+)
+performance_ci_df
+
+# # Plot Different Dataset Performance
+
+# ## Tune
+
+g = (
+    p9.ggplot(performance_ci_df >> ply.query("model=='tune'"))
+    + p9.aes(
+        x="lf_num",
+        y="aupr_mean",
+        ymin="aupr_lower_ci",
+        ymax="aupr_upper_ci",
+        group="label_source",
+        color="label_source",
+    )
+    + p9.geom_point()
+    + p9.geom_line()
+    + p9.geom_errorbar()
+    + p9.facet_wrap("~ prediction_label")
+    + p9.theme_bw()
+    + p9.theme(figure_size=(10, 10))
+    + p9.scale_color_brewer(type="qual", palette=2)
+)
+print(g)
+
+g = (
+    p9.ggplot(performance_ci_df >> ply.query("model=='tune'"))
+    + p9.aes(
+        x="lf_num",
+        y="auroc_mean",
+        ymin="auroc_lower_ci",
+        ymax="auroc_upper_ci",
+        group="label_source",
+        color="label_source",
+    )
+    + p9.geom_point()
+    + p9.geom_line()
+    + p9.geom_errorbar()
+    + p9.facet_wrap("~ prediction_label")
+    + p9.theme_bw()
+    + p9.theme(figure_size=(10, 10))
+    + p9.scale_color_brewer(type="qual", palette=2)
+)
+print(g)
+
+g = (
+    p9.ggplot(performance_ci_df >> ply.query("model=='tune'"))
+    + p9.aes(
+        x="lf_num",
+        y="bce_mean",
+        ymin="bce_lower_ci",
+        ymax="bce_upper_ci",
+        group="label_source",
+        color="label_source",
+    )
+    + p9.geom_point()
+    + p9.geom_line()
+    + p9.geom_errorbar()
+    + p9.facet_wrap("~ prediction_label")
+    + p9.theme_bw()
+    + p9.theme(figure_size=(10, 10))
+    + p9.scale_color_brewer(type="qual", palette=2)
+)
+print(g)
+
+# ## Test
+
+g = (
+    p9.ggplot(performance_ci_df >> ply.query("model=='test'"))
+    + p9.aes(
+        x="lf_num",
+        y="aupr_mean",
+        ymin="aupr_lower_ci",
+        ymax="aupr_upper_ci",
+        group="label_source",
+        color="label_source",
+    )
+    + p9.geom_point()
+    + p9.geom_line()
+    + p9.geom_errorbar()
+    + p9.facet_wrap("~ prediction_label")
+    + p9.theme_bw()
+    + p9.theme(figure_size=(10, 10))
+    + p9.scale_color_brewer(type="qual", palette=2)
+)
+print(g)
+
+g = (
+    p9.ggplot(performance_ci_df >> ply.query("model=='test'"))
+    + p9.aes(
+        x="lf_num",
+        y="auroc_mean",
+        ymin="auroc_lower_ci",
+        ymax="auroc_upper_ci",
+        group="label_source",
+        color="label_source",
+    )
+    + p9.geom_point()
+    + p9.geom_line()
+    + p9.geom_errorbar()
+    + p9.facet_wrap("~ prediction_label")
+    + p9.theme_bw()
+    + p9.theme(figure_size=(10, 10))
+    + p9.scale_color_brewer(type="qual", palette=2)
+)
+print(g)
+
+g = (
+    p9.ggplot(performance_ci_df >> ply.query("model=='test'"))
+    + p9.aes(
+        x="lf_num",
+        y="bce_mean",
+        ymin="bce_lower_ci",
+        ymax="bce_upper_ci",
+        group="label_source",
+        color="label_source",
+    )
+    + p9.geom_point()
+    + p9.geom_line()
+    + p9.geom_errorbar()
+    + p9.facet_wrap("~ prediction_label")
+    + p9.theme_bw()
+    + p9.theme(figure_size=(10, 10))
+    + p9.scale_color_brewer(type="qual", palette=2)
+)
+print(g)
